782477dcd92f95566d4be479461cbf33a7c24785
Suporte <junioralbino741@gmail.com>
2022-08-23 15:55:25 -0300

    Configurando o secret word

---
+++ src/data/word.js
@@ -0,0 +1,8 @@
+export const wordlist = {
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Antena" ],
+    fruta: ["Maça", "Banana", "Abacaxi", "Melancia", "Uva" ],
+    corpo: ["Cabeça", "Perna", "Braço", "Cabelo", "Orelha" ],
+    computador: ["Munitor", "Mouse", "Techado", "Gabinete", "Internet" ],
+    programação: ["PHP", "Laravel", "Codeighiter", "React", "React-native" ],
+    alimento: ["Arroz", "Feijão", "Batata", "Tomate", "Beterraba" ],
+};
