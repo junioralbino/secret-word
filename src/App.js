9f3e11a46df97d9bb014146fcc532b7d5d03831b
Suporte <junioralbino741@gmail.com>
2022-08-26 08:47:51 -0300

    Criando a estrutura do dados do jogo

--- src/App.js
+++ src/App.js
@@ -28,10 +28,39 @@ function App() {
   const [pickerWord,  setPickerWord] = useState("");
   const [pickerCategory, setPickerCategory ] = useState("");
   const [letters, setLetters] = useState([]);
+
+  const pickWordCategory = () => {
+    //pick random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    //pick random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return {word, category}
+  }
    
   //start the secret word game
   const startGame = () => {
-      setGameStage(stages[1].name);
+    const {word, category } = pickWordCategory();
+
+    //create an array of letters
+    let wordLetters = word.split("");
+    
+    console.log(word, category);
+    console.log(wordLetters);
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    setPickerWord(word);
+    setPickerCategory(category);
+    setLetters(letters);
+      
+    setGameStage(stages[1].name);
   }
 
   //process the letter input
--- src/components/Game.js
+++ src/components/Game.js
@@ -4,8 +4,29 @@ import './Game.css';
 function Game({verifyLetter}) {
   return (
     <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter} >Finalizar jogo</button>
+     <div className='game'>
+        <p className='points'>
+            <span>Pontuação: 000</span>
+        </p>
+        <h1>Advinhe a palavra:</h1>
+        <h3 className='tip'>
+          Dica sobre a palavra: <span>Dica....</span>
+        </h3>
+     </div>
+
+     <div className='wordContainer'>
+       <span className='letter'>A</span>
+       <span  className='blankSquare'></span> 
+     </div>
+
+     <div className='letterContainer'>
+       <p>Tente advinhar uma letra da palavra:</p>
+
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+     </div>
     </div>
   )
 }
